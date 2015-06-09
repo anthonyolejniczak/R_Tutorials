0d7c902883d19f337afd7de128e66adf7d85a28a
Anthony J. Olejniczak <aolejniczak@Anthonys-MacBook-Pro.local>
2015-06-08 23:03:32 -0400

    Messing around with scatterplots.

--- CSV_Manipulations/open_csv.r
+++ CSV_Manipulations/open_csv.r
@@ -2,9 +2,18 @@ require(pastecs)
 
 sink("myfile.txt", append=FALSE, split=TRUE)
 
-mydata = read.csv("mydata.csv")  # read csv file 
+mydata = read.csv("AAU_Full_Data.csv")  # read csv file 
 
-output = format(stat.desc(mydata["pub2013"]),5,5,scientific=FALSE)
+#output = format(stat.desc(mydata["totpubs"]),5,5,scientific=FALSE)
+
+num.site <- as.numeric(mydata[,"numfac"])
+
+plot(mydata$totcits, mydata$totpubs, main="Scatterplot Example", col=num.site, pch=16, cex=2)
+
+# Add fit lines
+abline(lm(mydata$totpubs ~ mydata$totcits), col=rgb(0,200,0,100,maxColorValue=255)) # regression line (y~x) 
+
+# lines(lowess(mydata$totcits, mydata$totpubs), col="blue") # lowess line (x,y)
 
 print(output)
 
